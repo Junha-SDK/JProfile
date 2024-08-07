2f2102568f7da77e53058a0e429fc2c2a3d01e85
jjunhaa0211 <goodjunha@gmail.com>
2024-08-07 14:09:30 +0900

    커밋

--- Sources/JProfile/JProfile.swift
+++ Sources/JProfile/JProfile.swift
@@ -1,2 +1,68 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+
+import SnapKit
+import RxCocoa
+import RxSwift
+
+@available(iOS 13.0, *)
+open class JProfileView: UIView {
+    
+    private var profileImageView = UIImageView()
+    public private(set) var profileImage: JProfileImage?
+    private let profileType: JProfileType
+    
+    public init (
+        profileImage: JProfileImage,
+        profileType: JProfileType
+    ) {
+        self.profileImage = profileImage
+        self.profileType = profileType
+        
+        super.init(frame: .zero)
+        
+        configureLayout()
+        configureImage(with: profileImage)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        profileImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        if let image = self.profileImage?.image {
+            self.profileImageView.image = image
+        }
+        
+        self.layer.cornerRadius = self.profileType.radius
+        self.clipsToBounds = true
+        
+        self.snp.makeConstraints { make in
+            make.width.height.equalTo(self.profileType.size)
+        }
+        
+    }
+    
+    private func configureLayout() {
+        addSubview(profileImageView)
+    }
+    
+    public func configureImage(with junhaProfileImage: JProfileImage) {
+        self.profileImage = junhaProfileImage
+        
+        switch junhaProfileImage.type {
+        case .custom:
+            if let customImg = junhaProfileImage.customImage {
+                self.profileImageView.image = customImg
+            } else {
+                self.profileImageView.image = UIImage(systemName: "square.and.arrow.up")!
+                self.backgroundColor = .gray
+            }
+        }
+    }
+}
--- Sources/JProfile/JProfileType.swift
+++ Sources/JProfile/JProfileType.swift
@@ -1,8 +1,71 @@
-//
-//  File.swift
-//  
-//
-//  Created by 박준하 on 8/7/24.
-//
-
 import Foundation
+import UIKit
+
+public enum JProfileType {
+    case smallProfile
+    case middleProfile
+    case bigProfile
+    case maxProfile
+    case intro
+    case bage
+    
+    var size: CGFloat {
+        switch self {
+        case .smallProfile:
+            return 36.0
+        case .middleProfile:
+            return 40.0
+        case .bigProfile:
+            return 48.0
+        case .intro:
+            return 280.0
+        case .bage:
+            return 16.0
+        case .maxProfile:
+            return 120.0
+        }
+    }
+    
+    var radius: CGFloat {
+        switch self {
+        case .smallProfile:
+            return 18.0
+        case .middleProfile:
+            return 20.0
+        case .bigProfile:
+            return 24.0
+        case .intro:
+            return 140.0
+        case .bage:
+            return 8.0
+        case .maxProfile:
+            return 60.0
+        }
+    }
+}
+
+public enum JProfileImageType: Int {
+    case custom
+}
+
+@available(iOS 13.0, *)
+public struct JProfileImage {
+    public let type: JProfileImageType
+    public var customImage: UIImage?
+    
+    public init(type: JProfileImageType, customImage: UIImage?) {
+        self.type = type
+        self.customImage = customImage
+    }
+    
+    var image: UIImage {
+        switch type {
+        case .custom:
+            guard let customImage = customImage else {
+                return UIImage(systemName: "square.and.arrow.up")!
+            }
+            
+            return customImage
+        }
+    }
+}
